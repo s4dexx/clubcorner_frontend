8a686478883fe66a7d150066847fe4da1e2f1770
schustern <niklas.schuster@hpe.com>
2018-05-27 16:07:20 +0200

    Home page

--- clubcorner/src/pages/home/home.ts
+++ clubcorner/src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  selectedItem: any;
+  icons: string[];
+  items: Array<{title: string}>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // If we navigated to this page, we will have an item available as a nav param
+    this.selectedItem = navParams.get('item');
 
+
+    this.items = [];
+    for (let i = 1; i < 5; i++) {
+      this.items.push({
+        title: 'Mannschaft ' + i,
+      });
+    }
   }
-
 }
